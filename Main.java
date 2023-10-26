02edd91b31414b97e202a6a02da406b94ea14c2c
ghaliah3074 <148686086+ghaliah3074@users.noreply.github.com>
2023-10-26 14:55:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,238 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//q1
+// int a=125;
+// int b=24;
+//        System.out.println(a+b+" ");
+//        System.out.println(a-b+" ");
+//        System.out.println(a%b+" ");
+//        System.out.println(a*b+" ");
+//        System.out.println(a+b+" ");
+
+
+//q2
+//        int c = 8;
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(c * i);
+//        }
+//
+        //q3
+        Scanner cc = new Scanner(System.in);
+//        System.out.println("pls enter r");
+//        double r=cc.nextDouble();
+//
+//        System.out.println(3.14*r);
+
+//q4
+//
+//             int n;
+//             float n2, avr, sum = 0;
+//
+//            System.out.println("enter the count of integer : ");
+//            n = cc.nextInt();
+//
+//            while ( n >= 0) {
+//
+//                System.out.println(" inter the number");
+//                n2 = cc.nextInt();
+//                if (n2 != 0) {
+//
+//                    sum += n2;
+//                    ++n;
+//                } else {
+//                    avr = sum /n;
+//                    System.out.println(n);
+//                    System.out.println("the average is " + avr);
+//                }
+//            }
+
+        //q5
+
+//        System.out.println("enter the first number: ");
+//        int x=cc.nextInt();
+//        System.out.println("enter the second number: ");
+//        int y=cc.nextInt();
+//        System.out.println("enter the third number: ");
+//        int z= cc.nextInt();
+//
+//
+//            boolean sum=x+y==z;
+//            System.out.println(x+y==z);
+//        }
+
+          //q6
+
+//        System.out.println("enter sentence :  ");
+//        String ss = cc.nextLine();
+//        ss=ss.trim();
+//        String result="";
+//
+//
+//        char[]  ch=ss.toCharArray();
+//        for( int i=ch.length-1; i>=0 ;i--){
+//            result +=ch[i];
+//        }
+//        System.out.println("reverse word : " +result.trim());
+
+
+     //q7
+//        System.out.println("enter a number : ");
+//        int num=cc.nextInt();
+//
+//          if (num %2==0){
+//              System.out.println("the number is even");
+//          }
+//          else {
+//              System.out.println("the number is odd");
+//          }
+
+
+        //q8
+
+//         double celsius =43,fahrenheit=0.0;
+//
+//
+//         fahrenheit=(celsius*1.8)+32;
+//        System.out.println("the temperature in celsius to fahrenheit : " +fahrenheit
+//        );
+
+
+        //q9
+
+//        System.out.println("enter your sentence:   ");
+//        String sen=cc.toString();
+//
+//
+//        System.out.println( "enter number index: ");
+//        int v =cc.nextInt();
+//        char x = sen.charAt(v);
+
+
+
+        //q10
+//        double parameter ;
+//        double area ;
+//
+//        System.out.println("enter length of rectangle: ");
+//        double l=cc.nextDouble();
+//        System.out.println("enter width of rectangle: ");
+//        double b=cc.nextDouble();
+//
+//        parameter=2*(l+b);
+//        System.out.println("the parameter of rectangle: "+parameter);
+//        area=l*b;
+//        System.out.println("the area of rectangle: "+area);
+
+
+        //q11
+//        System.out.println(" enter first number: ");
+//        int a=cc.nextInt();
+//
+//        System.out.println("enter second number");
+//        int aa=cc.nextInt();
+//
+//
+//        System.out.println(a>=aa);
+//        System.out.println(a<aa);
+//        System.out.println(a!=aa);
+//
+//
+
+        //q13
+//        System.out.println(" enter a first number :  ");
+//        int ff=cc.nextInt();
+//        System.out.println(" enter a second number :  ");
+//        int ee=cc.nextInt();
+//        System.out.println(" enter a third number :  ");
+//        int tt=cc.nextInt();
+//        System.out.println(" enter a four number :  ");
+//        int oo=cc.nextInt();
+//
+//       if(ff==ee && ee==tt&& tt==oo){
+//           System.out.println("number are equal ");
+//       }
+//      else{
+//           System.out.println("number are not equal");
+//       }
+//
+         //q14
+//        System.out.println("enter a number");
+//        int num=cc.nextInt();
+//
+//        if(num>0){
+//            System.out.println("the number is positive");
+//        }
+//        if (num<0){
+//            System.out.println("the number is negative");
+//        }
+//       if(num==0) {
+//            System.out.println("the number is zero");
+//        }
+
+
+
+        //q18
+        System.out.println(" enter spring :   ");
+        String sp=cc.nextLine();
+        char so = 0;
+
+
+
+            if (sp.charAt(so)==so) {
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
